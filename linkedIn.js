e3ec59d75bc0ce427f0f5dad7706e45d60b743e7
dav9go <david9go@gmail.com>
2024-05-13 09:57:38 +0200

    Eslint, logging steps and separate functionin files

---
+++ linkedIn.js
@@ -0,0 +1,63 @@
+async function getUserId(linkedInUserLocation) {
+  try {
+    const response = await fetch(linkedInUserLocation, {
+      headers: {
+        Authorization: `Bearer ${process.env.LINKEDIN_ACCESS_TOKEN}`,
+      },
+    });
+
+    if (response.ok) {
+      const userData = await response.json();
+      const userId = userData.sub;
+      return userId;
+    } else {
+      throw new Error("Failed to fetch user information from LinkedIn");
+    }
+  } catch (error) {
+    console.error("Error fetching user information:", error);
+    return null;
+  }
+}
+
+async function postInLinkedIn(
+  postObject,
+  linkedInUserLocation,
+  linkedInPostLocation
+) {
+  const userId = await getUserId(linkedInUserLocation);
+  const response = await fetch(linkedInPostLocation, {
+    method: "POST",
+    headers: {
+      "LinkedIn-Version": "202210",
+      "X-Restli-Protocol-Version": "2.0.0",
+      Authorization: `Bearer ${process.env.LINKEDIN_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      author: `urn:li:person:${userId}`,
+      lifecycleState: "PUBLISHED",
+      specificContent: {
+        "com.linkedin.ugc.ShareContent": {
+          shareCommentary: {
+            text: `${postObject.postTitle}`,
+          },
+          shareMediaCategory: "ARTICLE",
+          media: [
+            {
+              status: "READY",
+              originalUrl: `${postObject.postUrl}`,
+            },
+          ],
+        },
+      },
+      visibility: {
+        "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC",
+      },
+    }),
+  });
+  const responseText = await response.json();
+  return responseText;
+}
+
+module.exports = {
+  postInLinkedIn,
+};
---
+++ reddit.js
@@ -0,0 +1,82 @@
+//Get all reddit posts
+async function getRedditPosts(
+  url,
+  maxDescriptionLength,
+  maxPrevDateMiliseconds
+) {
+  console.log("Start: Fetching reddit posts");
+  try {
+    const callResult = await fetch(url);
+    const finalResult = await callResult.json();
+    if (finalResult.data.children.length > 0) {
+      const inDatePosts = filterMaxDatePosts(
+        finalResult.data.children,
+        maxPrevDateMiliseconds
+      );
+      const postChosen = findBestPost(inDatePosts);
+      return buildPost(postChosen, maxDescriptionLength);
+    } else {
+      throw new Error("No data found");
+    }
+  } catch (error) {
+    console.error("Error: Couldn't retrieve any posts: ", error);
+    return null;
+  }
+}
+
+function filterMaxDatePosts(postsArray, maxPrevDateMiliseconds) {
+  console.log("Start: Filtering reddit posts by date");
+  const todaysDate = new Date().getTime();
+  const milisecondDifference = todaysDate - maxPrevDateMiliseconds;
+  const insideDatePosts = postsArray.filter((post) => {
+    return todaysDate - post.data.created_utc < milisecondDifference;
+  });
+  return insideDatePosts;
+}
+
+//Get the best post
+function findBestPost(postsArrayFiltered) {
+  console.log("Start: Searching for the best post");
+  const bestPost = postsArrayFiltered.reduce((best, post) => {
+    return post.data.score > best.data.score ? post : best;
+  }, postsArrayFiltered[0]);
+
+  return bestPost;
+}
+
+//Convert to plain text
+function convertRawDescription(description, maxDescriptionLength) {
+  let plainText = description;
+  if (plainText.length > maxDescriptionLength) {
+    const maxLengthPlainText = plainText.substring(0, maxDescriptionLength);
+    const lastSpaceIndex = maxLengthPlainText.lastIndexOf(" ");
+    plainText = plainText.substring(0, lastSpaceIndex) + "...";
+  }
+  return plainText;
+}
+
+//Get all the necessary and processed information to post on LinkedIn
+function buildPost(data, maxDescriptionLength) {
+  const dataSource = data.data;
+  const postTitle = dataSource.title;
+  const rawPostDescription = dataSource.selftext;
+  const postDescription = convertRawDescription(
+    rawPostDescription,
+    maxDescriptionLength
+  );
+  const postUrl = dataSource.url;
+  console.log("Post built with: ");
+  console.log("postTitle: ", postTitle);
+  console.log("postDescription: ", postDescription);
+  console.log("postUrl: ", postUrl);
+  return {
+    postTitle,
+    rawPostDescription,
+    postDescription,
+    postUrl,
+  };
+}
+
+module.exports = {
+  getRedditPosts,
+};
